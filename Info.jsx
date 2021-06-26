95054ab26d76c85a9108bde4008a896b9dd4a6f3
kalyan-hub <65404384+kalyan-hub@users.noreply.github.com>
2021-06-26 17:17:18 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing Bootcamp conducted by Shaurya Sinha. We learnt
+        everything from the basics using Javascript, Reactjs, CSS, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
